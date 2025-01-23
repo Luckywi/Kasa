53f7cfc6ae1dcee821a200662695a1e514372052
luckywi <luckyleb@icloud.com>
2025-01-23 10:37:35 +0100

    90%

--- src/pages/logement.jsx
+++ src/pages/logement.jsx
@@ -1,7 +1,7 @@
 import { useParams, useNavigate} from 'react-router-dom';
 import logements from '../data/logements.json';
 import { useState } from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Star, ChevronDown } from 'lucide-react';
 
 function Logement() {
     const { id } = useParams(); // Récupère l'ID depuis l'URL
@@ -14,6 +14,22 @@ function Logement() {
     const decrement = () => {
         setCurrentIndex((currentIndex -1 + totalPhoto) % totalPhoto);
     }
+
+    const stars = [1, 2, 3, 4, 5];
+
+
+    const [openSections, setOpenSections] = useState([]); // On utilise un 
+    
+    const handleClick = (sectionName) => {  // sectionName sera soit "description" soit "equipements"
+        if (openSections.includes(sectionName)) {
+            setOpenSections(openSections.filter(name => name !== sectionName));
+        } else {
+            setOpenSections([...openSections, sectionName]);
+        }
+    }
+    
+
+
 
     
     
@@ -50,23 +66,66 @@ function Logement() {
         {logement.host.name.split(' ')[0]}<br/>
         {logement.host.name.split(' ')[1]}
         </h1>
-        <img src={logement.host.picture}></img>
+        <img src={logement.host.picture} alt={logement.host.name}></img>
         </div>
         </div>
 
+        <div className='tags-and-rating'>
 
-        <div className='tag-display'>
+    <div className='tag-display'>
         {logement.tags.map((tag) => (
-    <h4 key={tag} className='tag'>{tag}</h4>
+            <h4 key={tag} className='tag'>{tag}</h4>
         ))}
+    </div>
+
+    <div className='logement-rating'>
+        {stars.map((star) => (
+            <span key={star} className={star <= logement.rating ? "star-filled" : "star-empty"}><Star /></span>
+        ))}
+    </div>
 </div>
+
+
+<div className='logement-spec'>
+        <div className='collapse-section'>
+        <div className='collapse-header' onClick={() => handleClick("description")}>
+    <h1>Description</h1>
+    <ChevronDown className={`chevron ${openSections.includes("description") ? 'rotate' : ''}`}/>
+</div>
+
+            {openSections.includes("description") && (
+                <div className="collapse-content">
+                    <p>{logement.description}</p>
+                </div>
+            )}
+        </div>
+
+        <div className='collapse-section'>
+            <div className='collapse-header' onClick={() => handleClick("equipements")}>
+    <h1>Équipements</h1>
+    <ChevronDown className={`chevron ${openSections.includes("equipements") ? 'rotate' : ''}`}/>
+</div>
+
+            {openSections.includes("equipements") && (
+                <div className="collapse-content">
+                    {logement.equipments.map((equipement) => (
+                        <p key={`${logement.id}-equipment-`}>{equipement}</p>
+                    ))}
+                </div>
+            )}
+        </div>
 
             
 
-        
+
+            
+
+
+        </div>
+
+</div>
 
         
-        </div>
     );
 }
 
